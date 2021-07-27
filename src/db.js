c68edf4fa572aecb57e67e7264b6d7562b512f76
Zephyr Wong <zephyr.wong@qhms.com>
2021-07-27 10:26:06 +0800

    test: create campaign endpoint after db integration

--- src/db.js
+++ src/db.js
@@ -1,6 +1,12 @@
 const { Pool } = require('pg')
 
-const pool = new Pool()
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.NODE_ENV === 'test' ? process.env.PGDATABASE + '_test' : process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT
+})
 
 const PG_ERROR_CODE = {
   UNIQUE_VIOLATION: '23505'
--- test/api.test.js
+++ test/api.test.js
@@ -2,14 +2,15 @@ const request = require('supertest')
 
 const app = require('../src/app')
 const { validateCampaign } = require('../src/campaigns/validators')
+const { createCampaign } = require('../src/campaigns/queries')
 
 test('Hello World', async () => {
   const response = await request(app).get('/')
   expect(response.statusCode).toBe(200)
 })
 
-describe('Campaign', () => {
-  test('Create campaign', async () => {
+describe('Create campaign', () => {
+  test('Success case', async () => {
     const campaignToCreate = {
       title: 'What...?',
       start: '2021-07-24T00:00:00Z',
@@ -24,5 +25,21 @@ describe('Campaign', () => {
     expect(response.statusCode).toBe(201)
     validateCampaign(response.body)
     expect(validateCampaign.errors).toBeNull()
+  })
+
+  test('Invalid start and end', async () => {
+    const campaignToCreate = {
+      title: 'What...?',
+      start: '2021-07-25T00:00:00Z',
+      end: '2021-07-24T00:00:00Z',
+      candidates: [
+        {
+          title: 'option1'
+        }
+      ]
+    }
+    const response = await request(app).post('/campaigns').send(campaignToCreate)
+    expect(response.statusCode).toBe(400)
+    expect(response.body.errorCode === 'INVALID_PAYLOAD')
   })
 })
